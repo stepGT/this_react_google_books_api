eef6bfa5a82c9a0f1d87a94f887a408dc5345e4b
stepGT <stepGT@mail.ru>
2022-07-02 16:58:43 +1000

    Added dotenv, sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
-
+import './scss/App.module.scss';
+console.log(process.env);
 function App() {
   return (
-    <div className="App">
-      google_books_api
+    <div>
+      <small>
+        You are running this application in <b>{process.env.NODE_ENV}</b> mode.
+      </small>
+      <form>
+        <input type="hidden" defaultValue={process.env.REACT_APP_NOT_SECRET_CODE} />
+      </form>
     </div>
   );
 }
