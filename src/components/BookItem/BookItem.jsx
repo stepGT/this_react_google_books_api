2d17baecf44c8f13431e4d39921ca80515e579ef
stepGT <stepGT@mail.ru>
2022-07-03 18:32:19 +1000

    Styled BookItem

--- src/components/BookItem/BookItem.jsx
+++ src/components/BookItem/BookItem.jsx
@@ -2,16 +2,12 @@ import styles from './BookItem.module.scss';
 
 const BookItem = ({ volumeInfo: { title, imageLinks, authors, categories } }) => {
   return (
-    <div style={styles}>
-      <div>
-        <img src={imageLinks.thumbnail} alt="" />
-      </div>
-      <div>{categories && categories[0]}</div>
-      <div>
-        <h1>{title}</h1>
-      </div>
-      <div>
-        <h5>{authors && authors[0]}</h5>
+    <div className={styles.wrapper}>
+      <div className={styles.block}>
+        <img className={styles.image} src={imageLinks?.thumbnail} alt={title} />
+        <div className={styles.categories}>{categories?.[0]}</div>
+        <h1 className={styles.title}>{title}</h1>
+        <h3 className={styles.authors}>{authors?.[0]}</h3>
       </div>
     </div>
   );
