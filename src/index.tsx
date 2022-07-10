8a47840b8c55c2306e4c9764813728de891a2e71
stepGT <stepGT@mail.ru>
2022-07-11 01:07:24 +1000

    Added typescript

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import { store } from '@redux/store';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
